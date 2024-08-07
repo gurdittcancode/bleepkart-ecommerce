52c94fed784080dfcd9a0fdd3f45f2af9808e320
gurditt singh <gurdittsingh1508@gmail.com>
2024-08-07 21:04:44 +0530

    feat(cart): add cart types

---
+++ src/types/cart.d.ts
@@ -0,0 +1,12 @@
+import { Cart, Prisma } from '@prisma/client';
+
+export type CartWithProducts = Prisma.CartGetPayload<{
+  include: {
+    CartItem: { include: { product: true } };
+  };
+}>;
+
+export type ShoppingCart = CartWithProducts & {
+  size: number;
+  subTotal: number;
+};
