32363cfaa94a149619e777b69742673c9846bf67
gurditt singh <gurdittsingh1508@gmail.com>
2024-08-07 22:17:35 +0530

    misc: minor changes

--- src/components/ui/custom/ProductCard.tsx
+++ src/components/ui/custom/ProductCard.tsx
@@ -1,48 +1,51 @@
-import { FC } from 'react'
-import { Product } from '@prisma/client'
+import { FC } from 'react';
+import { Product } from '@prisma/client';
 import Link from 'next/link';
-import {Button} from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import {
-    Card,
-    CardContent,
-    CardImage,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-    CardDescription
+  Card,
+  CardContent,
+  CardImage,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  CardDescription,
 } from '@/components/ui/card';
 import PriceTag from './PriceTag';
 import { Badge } from '../badge';
 
 interface ProductCardProps {
-    product: Product;
+  product: Product;
 }
 
-const ProductCard: FC<ProductCardProps> = ({product}) => {
-
-    const isNew = Date.now() - new Date(product.createdAt).getTime() < 1000 * 60 * 60 * 24 * 7;
+const ProductCard: FC<ProductCardProps> = ({ product }) => {
+  const isNew =
+    Date.now() - new Date(product.createdAt).getTime() <
+    1000 * 60 * 60 * 24 * 7;
 
   return (
     <Link href={`/products/${product.id}`}>
-        <Card className='w-[300px] md:w-[350px] rounded-xl bg-secondary hover:shadow-xl transition-shadow'>
-            <CardImage 
-                alt={product.name}
-                src={product.imageUrl}
-                width={800}
-                height={400}
-                className='object-cover rounded-xl'
-            />
-            <CardHeader>
-                <CardTitle className='text-primary flex'>{product.name}
-                </CardTitle>
-                {isNew && <Badge className='bg-ring w-fit'>New</Badge>}
-                <CardDescription>{product.description.substring(0,200) + '...'}
-                </CardDescription>
-                <PriceTag price={product.price}/>
-            </CardHeader>
-        </Card>
+      <Card className="w-[300px] md:w-[350px] rounded-xl bg-secondary hover:shadow-xl transition-shadow">
+        <CardImage
+          alt={product.name}
+          src={product.imageUrl}
+          width={800}
+          height={400}
+          className="object-cover rounded-xl"
+        />
+        <CardHeader>
+          <CardTitle className="text-primary flex">
+            {product.name}
+          </CardTitle>
+            {isNew && <Badge className="bg-ring w-fit">New</Badge>}
+          <CardDescription>
+            {product.description.substring(0, 200) + '...'}
+          </CardDescription>
+          <PriceTag price={product.price} />
+        </CardHeader>
+      </Card>
     </Link>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
