86c7bb180705afcce7e606d3f766ecb9e5f3f61b
gurditt singh <gurdittsingh1508@gmail.com>
2024-08-07 21:00:06 +0530

    feat: create product page

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,38 +1,67 @@
 import PriceTag from '@/components/ui/custom/PriceTag';
-import { prisma } from '@/utils/db/prisma'
+import { prisma } from '@/utils/db/prisma';
+import { Metadata } from 'next';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
-import { FC } from 'react'
+import { cache, FC } from 'react';
+import AddToCartButton from './AddToCartButton';
 
 interface pageProps {
   params: {
-    productId: string,
-  }
+    productId: string;
+  };
+}
+
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+  if (!product) notFound();
+  return product;
+});
+
+export async function generateMetadata({
+  params: { productId },
+}: {
+  params: { productId: string };
+}): Promise<Metadata> {
+  const product = await getProduct(productId);
+  return {
+    title: `${product.name}- BleepKart`,
+    description: product.description,
+    openGraph: {
+      images: [
+        {
+          url: product.imageUrl,
+        },
+      ],
+    },
+  };
 }
 
 const page: FC<pageProps> = async ({ params }) => {
   const { productId } = params;
-  const product = await prisma.product.findUnique({
-    where: { id: productId },
-  });
-  if (!product) notFound();
-  
-  return <div className='flex flex-col lg:flex-row'>
-   <Image 
-      src={product.imageUrl}
-      alt={product.name}
-      width={500}
-      height={500}
-      className='rounded-lg'
-      priority
-    />
+  const product = await getProduct(productId);
 
-    <div>
-      <h1 className='text-5xl font-extrabold'>{product.name}</h1>
-      <PriceTag price={product.price} className='mt-4'/>
-      <p>{product.description}</p>
+  return (
+    <div className="flex flex-col lg:flex-row gap-4 lg:items-center">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
+
+      <div>
+        <h1 className="text-5xl font-extrabold">{product.name}</h1>
+        <PriceTag price={product.price} className="my-4" />
+        <p>{product.description}</p>
+        <AddToCartButton productId={product.id} className="mt-5" />
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
-export default page
+export default page;
