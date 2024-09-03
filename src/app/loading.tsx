21d56f912eaf9bf61fc71a3f49d9bb7358aceceb
gurditt singh <gurdittsingh1508@gmail.com>
2024-09-03 18:05:01 +0530

    add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import { Loader2 } from "lucide-react";
+import { FC } from "react";
+
+const loading: FC = ({}) => {
+  return <Loader2 className="ml-2 size-20 animate-spin m-auto" />
+}
+
+export default loading;
