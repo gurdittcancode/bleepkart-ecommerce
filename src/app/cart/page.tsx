cbdc444b5ae0a455761ab660f017b5a265e010cc
gurditt singh <gurdittsingh1508@gmail.com>
2024-10-13 12:58:24 +0530

    minor changes

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,40 +1,40 @@
-import { getCart } from '@/lib/db/cart';
-import { Metadata } from 'next';
-import { FC } from 'react';
-import CartItem from '@/components/CartItem';
-import { formatPrice } from '@/lib/format';
-import { Button } from '@/components/ui/button';
-import { Landmark } from 'lucide-react';
+import { getCart } from "@/lib/db/cart";
+import { Metadata } from "next";
+import { FC } from "react";
+import CartItem from "@/components/CartItem";
+import { formatPrice } from "@/lib/format";
+import { Button } from "@/components/ui/button";
+import { Landmark } from "lucide-react";
 
 interface PageProps {}
 
 export const metadata: Metadata = {
-  title: 'Your Cart | BleepKart',
+    title: "Your Cart | BleepKart",
 };
 
 const page: FC<PageProps> = async ({}) => {
-  const cart = await getCart();
+    const cart = await getCart();
 
-  return (
-    <div className="w-full">
-      <h1 className="text-3xl font-extrabold mb-6">YOUR CART</h1>
-      <div>
-        {cart?.CartItem.map((item) => (
-          <CartItem key={item.productId} cartItem={item} />
-        ))}
-      </div>
-      {!cart?.CartItem.length && <p>Your cart is empty</p>}
-      <div>
-        <p className="mb-3 font-bold">
-          Total: {formatPrice(cart?.subTotal || 0)}
-        </p>
-        <Button className="flex gap-2">
-          Checkout
-          <Landmark />
-        </Button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="w-full">
+            <h1 className="text-3xl font-extrabold mb-6">YOUR CART</h1>
+            <div>
+                {cart?.CartItem.map((item) => (
+                    <CartItem key={item.productId} cartItem={item} />
+                ))}
+            </div>
+            {!cart?.CartItem.length && <p>Your cart is empty</p>}
+            <div>
+                <p className="mb-3 font-bold">
+                    Total: {formatPrice(cart?.subTotal || 0)}
+                </p>
+                <Button className="flex gap-2">
+                    Checkout
+                    <Landmark />
+                </Button>
+            </div>
+        </div>
+    );
 };
 
 export default page;
